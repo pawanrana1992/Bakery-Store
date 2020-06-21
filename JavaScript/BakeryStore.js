e6056d351f5b24a13fa7e6113a9458fc5e199de8
Anil rana <anilabc.rana@gmail.com>
2020-06-21 17:12:03 +0530

    Initial commit

---
+++ JavaScript/BakeryStore.js
@@ -0,0 +1,8 @@
+function setNewImage() {
+	(document.getElementById('img-change').src = './images/Cakes/cake-3.jpeg'),
+		'./images/Cupcakes/cupcake-3.jpeg';
+}
+
+function setOldImage() {
+	document.getElementById('img-change').src = './images/Sweets/sweets-1.jpeg';
+}
